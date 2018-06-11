e1cca5c75851737d8cb330854737fbbfa96a3b60
basusaswata <basu.saswata@gmail.com>
2018-06-10 19:35:47 -0500

    1

--- src/main/java/com/tw/mc/mem/ConfigService/ConfigMonitorConfiguration.java
+++ src/main/java/com/tw/mc/mem/ConfigService/ConfigMonitorConfiguration.java
@@ -1,13 +1,17 @@
 package com.tw.mc.mem.ConfigService;
 
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.cloud.config.monitor.GitlabPropertyPathNotificationExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Profile("monitor")
+@Profile("config-monitor")
 @Configuration
 @Import(RabbitAutoConfiguration.class)
 public class ConfigMonitorConfiguration {
 
+	
 }
+
--- src/main/java/com/tw/mc/mem/ConfigService/ConfigServiceApplication.java
+++ src/main/java/com/tw/mc/mem/ConfigService/ConfigServiceApplication.java
@@ -3,7 +3,9 @@ package com.tw.mc.mem.ConfigService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.cloud.config.monitor.GitlabPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = { RabbitAutoConfiguration.class })
 @EnableConfigServer
@@ -11,5 +13,10 @@ public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+	
+	@Bean
+	public GitlabPropertyPathNotificationExtractor gitlabPropertyPathNotificationExtractor() {
+	  return new GitlabPropertyPathNotificationExtractor();
 	}
 }
